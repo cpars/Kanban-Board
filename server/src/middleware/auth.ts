d844314b94616f5ba54e066054348548f7d49a05
Corey P <cparsons0730@yahoo.com>
2025-03-06 21:53:07 -0600

    Setup authentication Middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,5 +6,25 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+  // Verify the token and check if it is valid
+  const authHeader = req.headers['authorization'];
+
+  // Extract the token from the Authorization header
+  const token = authHeader && authHeader.split(' ')[1];
+
+  // Check if the token is not present
+  if (token == null) {
+    return res.sendStatus(401).json({ error: 'Unauthorized' });
+  } else {
+    // Verify the token
+    jwt.verify(token, process.env.JWT_SECRET_KEY as string, (err, user) => {
+      if (err) {
+        return res.sendStatus(403).json({ message: 'Token expired or not valid' });
+      }
+      // Set the user in the request object
+      req.user = user as JwtPayload;
+      next();
+    });
+  }
 };
+  
