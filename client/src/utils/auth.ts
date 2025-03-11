9099ed841a1966ad1882e9457149886bbeeb0244
Corey P <cparsons0730@yahoo.com>
2025-03-10 20:47:25 -0500

    .env and isTokenExpired()

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,4 +1,4 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
 class AuthService {
   getProfile() {
@@ -11,40 +11,39 @@ class AuthService {
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token);
   }
-  
+
   isTokenExpired(token: string) {
+    // Decode the token
+    try {
       // Decode the token
       const decoded = jwtDecode<JwtPayload>(token);
       // Check if the token is expired
       if (decoded.exp && decoded.exp < Date.now() / 1000) {
         return true;
-      } else {
-        return false;
       }
+    } catch (err) {
+      return false;
     }
-  
+  }
 
   getToken(): string {
     // Get the token from localStorage
-    const token = localStorage.getItem('token');
-    if (!token) {
-      throw new Error('No token found');
-    }
+    const token = localStorage.getItem("token") || "";
     return token;
   }
 
   login(idToken: string) {
     // Set the token to localStorage
-    localStorage.setItem('token', idToken);
+    localStorage.setItem("token", idToken);
     // Redirect to the home page
-    window.location.assign('/');
+    window.location.assign("/");
   }
 
   logout() {
     // Remove the token from localStorage
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     // Redirect to the login page
-    window.location.assign('/login');
+    window.location.assign("/login");
   }
 }
 
